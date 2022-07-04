1769c3fa47b9c5d2fbaf8135265b40a807749315
Mark Reynolds <20478032+ers2015112@users.noreply.github.com>
2022-07-04 11:06:12 +0100

    DAC6-1858: Add upscan services and repositories

---
+++ app/services/upscan/UpScanCallbackDispatcher.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services.upscan
+
+import javax.inject.Inject
+import models.upscan._
+import play.api.Logging
+
+import scala.concurrent.Future
+
+class UpScanCallbackDispatcher @Inject() (sessionStorage: UploadProgressTracker) extends Logging {
+
+  def handleCallback(callback: CallbackBody): Future[Boolean] = {
+    val uploadStatus = callback match {
+      case s: ReadyCallbackBody =>
+        UploadedSuccessfully(
+          s.uploadDetails.fileName,
+          s.uploadDetails.fileMimeType,
+          s.downloadUrl,
+          Some(s.uploadDetails.size)
+        )
+      case s: FailedCallbackBody if s.failureDetails.failureReason == "QUARANTINE" =>
+        logger.warn(s"FailedCallbackBody, QUARANTINE: $s")
+        Quarantined
+      case s: FailedCallbackBody if s.failureDetails.failureReason == "REJECTED" =>
+        logger.warn(s"FailedCallbackBody, REJECTED: $s")
+        UploadRejected(s.failureDetails)
+      case f: FailedCallbackBody =>
+        logger.warn(s"FailedCallbackBody: $f")
+        Failed
+    }
+    sessionStorage.registerUploadResult(callback.reference, uploadStatus)
+  }
+
+}
