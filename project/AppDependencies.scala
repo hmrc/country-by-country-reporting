715f7d74453b4524efafc57984a2f72cc236a730
Mahipal Padala <22635253+mahipal-padala@users.noreply.github.com>
2022-07-26 15:02:43 +0100

    DAC6-1910: Updated mongo play 28 (#5)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,14 +7,14 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.24.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.63.0"
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.68.0"
   )
 
   val test = Seq(
     "org.scalatestplus"       %% "scalatestplus-scalacheck"   % "3.1.0.0-RC2"       % Test,
     "wolfendale"              %% "scalacheck-gen-regexp"      % "0.1.2"             % Test,
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.24.0"            % "test, it",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.63.0"            % Test,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.68.0"            % Test,
     "org.mockito"             %% "mockito-scala"              % "1.10.6"            % Test,
     "org.scalatest"           %% "scalatest"                  % "3.1.0"             % Test,
     "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0"             % Test,
