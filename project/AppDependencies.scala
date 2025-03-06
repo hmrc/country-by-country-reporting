9a4b2bb0b854a2f0b45e6db1a1506f77818db552
jayandran-Sampath <24522469+jayandran-Sampath@users.noreply.github.com>
2025-03-06 15:38:16 +0000

    update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object AppDependencies {
-  private val mongoVersion     = "2.2.0"
-  private val bootstrapVersion = "9.5.0"
+  private val mongoVersion     = "2.5.0"
+  private val bootstrapVersion = "9.11.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
