15c1cdc049079c48b92050689aa81bc3e4db9ae6
jayandran-Sampath <24522469+jayandran-Sampath@users.noreply.github.com>
2026-04-16 08:45:25 +0100

    DAC6-4203 | fix incorrect log

--- app/utils/CustomAlertUtil.scala
+++ app/utils/CustomAlertUtil.scala
@@ -28,7 +28,7 @@ class CustomAlertUtil extends Logging {
   def alertForProblemStatus(errors: ValidationErrors): Unit = {
     val errorCodes = Seq(errors.fileError.map(_.map(_.code.code)).getOrElse(Nil), errors.recordError.map(_.map(_.code.code)).getOrElse(Nil)).flatten
     if (errorCodes.exists(problemsStatusErrorCodes.contains(_))) {
-      logger.warn(s"File Rejected with unexpected errors codes returned: ${errorCodes.map(_.mkString(" and "))}")
+      logger.warn(s"File Rejected with unexpected errors codes returned: ${errorCodes.mkString(" and ")}")
     }
   }
 
